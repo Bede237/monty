fb7837085699d910642143eca99a82a5f54e5e81
Bede237 <byounkwe@gmail.com>
2023-09-21 13:19:06 -0700

    all files

---
+++ func.c
@@ -0,0 +1,128 @@
+#include "monty.h"
+/**
+ * getval - gets push val
+ * @list: the list
+ * @ln: lin num
+ *
+ * Return: num
+ */
+int getval(char *list, int ln)
+{
+	char *op_cd = "push";
+
+	while (*list != '\0')
+	{
+		if (*op_cd == *list)
+		{
+			list++;
+			op_cd++;
+			if (*op_cd == '\0')
+			{
+				while (*list)
+				{
+					if ((*list >= '0' && *list <= '9') || (*list == '-'))
+					{
+						check_num(list, ln);
+						return (atoi(list));
+					}
+					else if (*list == ' ')
+					{
+						list++;
+					}
+					else
+					{
+						fprintf(stderr, "L%d: usage: push integer\n", ln);
+						exit(EXIT_FAILURE);
+					}
+				}
+			}
+		}
+		else
+			list++;
+	}
+	return (0);
+}
+/**
+ * check_num - cheks num
+ * @list: list to check
+ * @ln: line num
+ *
+ * Return 1
+ */
+int check_num(char *list, int ln)
+{
+	while (*list)
+	{
+		if (*list == '\0' || *list == '\n')
+		{
+			break;
+		}
+		else if ((*list >= '0' && *list <= '9') || (*list == ' ') || (*list == '-'))
+			list++;
+		else
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", ln);
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (1);
+}
+/**
+ * newline - checks for new line char
+ * @pt: string
+ *
+ * Return: 1(success)
+ */
+int newline(char *pt)
+{
+	char *p = "\n";
+	int i = 0;
+
+	while (pt[i] != '\0')
+	{
+		if (*p == pt[i])
+		{
+			p++;
+			i++;
+			if (*p == '\0')
+			{
+				return (1);
+			}
+		}
+		else
+			i++;
+	}
+	return (0);
+}
+/**
+ * free_com - free com
+ * @p: pointer
+ */
+void free_com(char *p)
+{
+	int i = 0;
+
+	while (p[i] != NULL)
+	{
+		free(p[i]);
+		i++;
+	}
+	free(p);
+}
+/**
+ * free_stack - free list
+ * @head: pointer
+ */
+void free_stack(stack_t *head)
+{
+	stack_t *temp = head;
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
---
+++ monty.c
@@ -0,0 +1,110 @@
+#include "monty.h"
+/**
+ * main - Entry point
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: 0(success)
+ */
+int main(int argc, char **argv)
+{
+	stack_t *head = NULL;
+	static char *content[1024] = NULL;
+	FILE *fd;
+	int n = 0;
+	size_t len = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fd = fopen(argv[1], "r");
+	if (fd = NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&(content[n]), &len, fd) != -1)
+		n++;
+	handle(content, head);
+	free(content);
+	fclose(fd);
+	return (0);
+}
+
+/**
+ * handle - executes opcodes
+ * @str: contents of file
+ * @head: head of list
+ */
+void handle(char *str, stack_t *head)
+{
+	int ln, n, i, lp;
+
+	instruction_t st[] = 
+	{
+		{"pall", pall},
+		{"add", add},
+		{"pop", pop},
+		{"swap", swap},
+		{"nop", nop},
+		{"pint", pint},
+		{NULL, NULL}
+	};
+
+	for (ln = 1, n = 0; str[n] != NULL; ln++, n++)
+	{
+		if (_strcmp("push", str[n]))
+		{
+			lp = getval(str[n], ln);
+			push(&head, ln, lp);
+		}
+		else
+		{
+			i = 0;
+			while (st[i].opcode != NULL)
+			{
+				if (_strcmp(st[i].opcode, str[n]))
+				{
+					st[i].f(&head, ln);
+					break;
+				}
+				i++;
+			}
+			if ((st[i].opcode == NULL) && !(_strcmp(str[n], "\n")))
+			{
+				fprintf(stderr, "l%u:  unknown instruction %s", ln, str[n]);
+				if (!newline(str[n]))
+					perror("\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
+
+/**
+ * _strcmp - compares strings
+ * @p: string 1
+ * @s: string 2
+ *
+ * Return: 1(success)
+ */
+int _strcmp(char *p, char *s)
+{
+	while (*s != '\0')
+	{
+		if (*s == ' ')
+		{
+			s++;
+		}
+		else if (*p == *s)
+		{
+			p++;
+			s++;
+			if (*p == '\0' && (*s == ' ' || *s == "\n" || *s == '\0'))
+				return (1);
+		}
+	}
+	return (0);
+}
